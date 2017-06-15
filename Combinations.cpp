779e54adb87923f19c283ad92a9c419747fc4373
iRoy7 <myoungwoon@gmail.com>
2017-06-15 21:50:55 +0900

    Add comments and  new functions

--- Combinations.cpp
+++ Combinations.cpp
@@ -1,11 +1,20 @@
+/*
+* Generate combinations
+*/
+
 #include <stdio.h>
-const int NMAX = 5;
+const int NMAX = 4;
 
 int Src[NMAX] = { 1, 2, 3, 4 };
-int Sel[NMAX];
-int N = 4;
+int Sel[NMAX+1];
 int cnt;
 
+/*
+* Generate Combination Set
+* @ param s Start number (0 <= Src[s] < NMAX)
+* @ param k Selected sequence (0 <= Sel[k] < q)
+* @ param q Target number
+*/
 void Combi_Set_L(int s, int k, int q)
 {
 	if (k == q)
@@ -17,26 +26,59 @@ void Combi_Set_L(int s, int k, int q)
 		return;
 	}
 
-	for (int i = s; i < N; i++)
+	// i is started from 's' unlike permutation has started from '0'
+	for (int i = s; i < NMAX; i++)
 	{
 		Sel[k] = i;
 		Combi_Set_L(i + 1, k + 1, q);
 	}
 }
 
-void RCombi_Set_L(int s, int k, int q)
+/*
+* Generate Combination Set
+* @ param s Start number (0 <= Src[s] < NMAX)
+* @ param k Selected sequence (1 <= Sel[k] <= q)
+* @ param q Target number
+*/
+void Combi_Set_L2(int s, int k, int q)
 {
-	if (k == q)
+	if (k > q)
 	{
 		cnt++;
-		for (int i = 0; i < q; i++)
+		for (int i = 1; i <= q; i++)
 			printf("%d ", Src[Sel[i]]);
 		printf("\n");
 		return;
 	}
 
-	for (int i = s; i < N; i++)
+	// i is started from 's' unlike permutation has started from '0'
+	for (int i = s; i < NMAX; i++)
+	{// i: Src index: 0, 1, 2 3
+		Sel[k] = i;
+		Combi_Set_L2(i + 1, k + 1, q);
+	}
+}
+
+/*
+* Generate Repetition Combination Set
+* @ param s Start number (0 <= Src[s] < NMAX)
+* @ param k Selected sequence (1 <= Sel[k] <= q)
+* @ param q Target number
+*/
+void RCombi_Set_L(int s, int k, int q)
+{
+	if (k > q)
 	{
+		cnt++;
+		for (int i = 1; i <= q; i++)
+			printf("%d ", Src[Sel[i]]);
+		printf("\n");
+		return;
+	}
+
+	// Keep i unlike general combination increase i. (i+1)
+	for (int i = s; i < N; i++)
+	{// i: Src index: 0, 1, 2 3
 		Sel[k] = i;
 		RCombi_Set_L(i, k + 1, q);
 	}
@@ -48,6 +90,12 @@ void print_trr(int q)
 	printf("{ %d %d %d }\n", trr[0], trr[1], trr[2]);
 }
 
+/*
+* Generate Repetition Combination Set
+* @ param n Start number (NMAX > Src[s] >= 0)
+* @ param r Selected number (Target > Sel[k] >= 0)
+* @ param q Target number
+*/
 void Combi_Set_AL(int n, int r, int q)
 {
 	if (r == 0)
